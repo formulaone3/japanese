2c0946c27b6df4d4f72570b49802bd709c602e2a
formulaone3 <110986793+formulaone3@users.noreply.github.com>
2023-01-31 11:05:44 +0000

    s

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-import kanjiArray from 'kanji.js'
-import name from 't.js'
-alert(name)
-alert(kanjiArray[0])
+//import kanjiArray from 'kanji.js'
+
+
 const root = document.getElementById('root')
 const kanjiInput = document.getElementById('kanji-input')
-const submitBtn = document.getElementById('submit-button')
 const currentKanji = document.getElementById('kanji-text')
+const wrongInput = document.getElementById('wrong-input')
 
-//const kanjiArray = ["気", "目", "木", "花", "海", "人", "一つ", "一人", "二人", "三日", "五", "四", "六", "七", "八", "九", "十", "百", "万", "千", "日", "週", "月", "年", "時", "間", "分", "午後", "午前", "先","来る", "何", "半", "今", "男"]
+const kanjiArray = ["気", "目", "木", "花", "海", "人", "一つ", "一人", "二人", "三日", "五", "四", "六", "七", "八", "九", "十", "百", "万", "千", "日", "週", "月", "年", "時", "間", "分", "午後", "午前", "先","来る", "何", "半", "今", "男"]
 
 
 currentKanji.innerHTML = kanjiArray[0]
 
 
-function updateKanji(){
-    if(kanjiInput.value == currentKanji.innerHTML){
-        currentKanji.innerHTML = kanjiArray[Math.floor(Math.random() * kanjiArray.length)]
-        kanjiInput.value = ""
-    }
-    
-}
+//function updateKanji()
 
-submitBtn.addEventListener('click', updateKanji)
+kanjiInput.addEventListener('keypress', ()=>{
+        if (event.key === "Enter"){
+            if(kanjiInput.value == currentKanji.innerHTML){
+                currentKanji.innerHTML = kanjiArray[Math.floor(Math.random() * kanjiArray.length)]
+                kanjiInput.value = ""
+                wrongInput.innerHTML = ""
+            } else {
+                wrongInput.innerHTML = "Wrong"
+            }
+    } 
+})
+
