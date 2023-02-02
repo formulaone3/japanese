cc2ef74b6ad3b55322545793e37f1808fe1e5126
formulaone3 <110986793+formulaone3@users.noreply.github.com>
2023-02-02 11:26:35 +0000

    importing kanji

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {kanjiArrayn1, kanjiArrayn2, kanjiArrayn3, kanjiArrayn4, kanjiArrayn5} from './kanjiArrays.js'
+
 let kanjiInput = document.getElementById('kanji-input')
 let currentKanji = document.getElementById('kanji-text')
 let wrongInput = document.getElementById('wrong-input')
@@ -7,8 +9,7 @@ let btn3 = document.getElementById('btn-3')
 let btn4 = document.getElementById('btn-4')
 let btn5 = document.getElementById('btn-5')
 
-const kanjiArrayn5 = ["気", "目", "木", "花", "海", "人", "一つ", "一人", "二人", "三日", "五", "四", "六", "七", "八", "九", "十", "百", "万", "千", "日", "週", "月", "年", "時", "間", "分", "午後", "午前", "先","来る", "何", "半", "今", "男"]
-const kanjiArrayn4 = ["会", "同", "事", "社", "初", "物", "地", "場", "員", "開", "明", "動", "", "東", "体", "通", "言う", "話", "題", "度", "生", "安", "家", "世", "多い", "思う", "以上", "心", "重い", "元","文字", "世界", "近い", "遠い", "売る"]
+      
 
 let activeArray = null
 
@@ -33,3 +34,18 @@ btn4.addEventListener('click', () => {
   activeArray = kanjiArrayn4
   currentKanji.textContent = activeArray[Math.floor(Math.random() * activeArray.length)]
 })
+
+btn3.addEventListener('click', () => {
+    activeArray = kanjiArrayn3
+    currentKanji.textContent = activeArray[Math.floor(Math.random() * activeArray.length)]
+  })
+
+  btn2.addEventListener('click', () => {
+    activeArray = kanjiArrayn2
+    currentKanji.textContent = activeArray[Math.floor(Math.random() * activeArray.length)]
+  })
+
+  btn1.addEventListener('click', () => {
+    activeArray = kanjiArrayn1
+    currentKanji.textContent = activeArray[Math.floor(Math.random() * activeArray.length)]
+  })
