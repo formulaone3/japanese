36d8f2b349d80b6cac08fb8af18078b8d235a8fc
formulaone3 <110986793+formulaone3@users.noreply.github.com>
2023-02-02 10:46:27 +0000

    still stuck with wrong

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ let btn5 = document.getElementById('btn-5')
 const kanjiArrayn5 = ["気", "目", "木", "花", "海", "人", "一つ", "一人", "二人", "三日", "五", "四", "六", "七", "八", "九", "十", "百", "万", "千", "日", "週", "月", "年", "時", "間", "分", "午後", "午前", "先","来る", "何", "半", "今", "男"]
 const kanjiArrayn4 = ["会", "同", "事", "社", "初", "物", "地", "場", "員", "開", "明", "動", "", "東", "体", "通", "言う", "話", "題", "度", "生", "安", "家", "世", "多い", "思う", "以上", "心", "重い", "元","文字", "世界", "近い", "遠い", "売る"]
 
-btn5.addEventListener('click', newKanji(kanjiArrayn5))
-btn4.addEventListener('click', newKanji(kanjiArrayn4))
-
 function newKanji(array){
     currentKanji.textContent = array[Math.floor(Math.random() * array.length)]
     kanjiInput.addEventListener('keypress', ()=>{
@@ -21,10 +18,13 @@ function newKanji(array){
                 kanjiInput.value = ""
                 wrongInput.textContent = ""
                 currentKanji.textContent = array[Math.floor(Math.random() * array.length)]
-            } else{
-                wrongInput.textContent = "Wrong"
+                
+            } else {
+                wrongInput.textContent = "wrong"
             }
     } 
-    alert(currentKanji.textContent)
 })
 }
+
+btn5.addEventListener('click', ()=> newKanji(kanjiArrayn5))
+btn4.addEventListener('click', ()=> newKanji(kanjiArrayn4))
