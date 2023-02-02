ed222b7480e05ea2414ad723d907ba1cde84eb56
formulaone3 <110986793+formulaone3@users.noreply.github.com>
2023-02-02 10:54:26 +0000

    chatgpt fix
    The issue with this code is that every time the 'btn5' or 'btn4' buttons are clicked, a new keypress event listener is added to the 'kanjiInput' element. This means that every time the button is clicked, there will be another listener active, even if a previous one is still running. This will cause multiple event listeners to be triggered when the 'Enter' key is pressed, which is likely not desired. To fix this, the keypress event listener should be added only once, for example, outside of the newKanji() function.

--- index.js
+++ index.js
@@ -10,21 +10,26 @@ let btn5 = document.getElementById('btn-5')
 const kanjiArrayn5 = ["気", "目", "木", "花", "海", "人", "一つ", "一人", "二人", "三日", "五", "四", "六", "七", "八", "九", "十", "百", "万", "千", "日", "週", "月", "年", "時", "間", "分", "午後", "午前", "先","来る", "何", "半", "今", "男"]
 const kanjiArrayn4 = ["会", "同", "事", "社", "初", "物", "地", "場", "員", "開", "明", "動", "", "東", "体", "通", "言う", "話", "題", "度", "生", "安", "家", "世", "多い", "思う", "以上", "心", "重い", "元","文字", "世界", "近い", "遠い", "売る"]
 
-function newKanji(array){
-    currentKanji.textContent = array[Math.floor(Math.random() * array.length)]
-    kanjiInput.addEventListener('keypress', ()=>{
-        if (event.key === "Enter"){
-            if(kanjiInput.value == currentKanji.textContent){
-                kanjiInput.value = ""
-                wrongInput.textContent = ""
-                currentKanji.textContent = array[Math.floor(Math.random() * array.length)]
-                
-            } else {
-                wrongInput.textContent = "wrong"
-            }
-    } 
-})
-}
+let activeArray = null
 
-btn5.addEventListener('click', ()=> newKanji(kanjiArrayn5))
-btn4.addEventListener('click', ()=> newKanji(kanjiArrayn4))
+kanjiInput.addEventListener('keypress', () => {
+  if (event.key === "Enter") {
+    if (kanjiInput.value == currentKanji.textContent) {
+      kanjiInput.value = ""
+      wrongInput.textContent = ""
+      currentKanji.textContent = activeArray[Math.floor(Math.random() * activeArray.length)]
+    } else {
+      wrongInput.textContent = "wrong"
+    }
+  }
+})
+
+btn5.addEventListener('click', () => {
+  activeArray = kanjiArrayn5
+  currentKanji.textContent = activeArray[Math.floor(Math.random() * activeArray.length)]
+})
+
+btn4.addEventListener('click', () => {
+  activeArray = kanjiArrayn4
+  currentKanji.textContent = activeArray[Math.floor(Math.random() * activeArray.length)]
+})
